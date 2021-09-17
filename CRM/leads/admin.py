bdf9c965d8235e8b35476f6f5a13b5b59fbe613b
khasbin <khasbin@gmail.com>
2021-09-17 21:11:37 +0545

    adds model for user agent and lead

--- CRM/leads/admin.py
+++ CRM/leads/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from leads.models import Lead, Agent, User
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Agent)
+admin.site.register(Lead)
+
+
+
