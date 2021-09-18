daed226a78de1c10ab9e940c9a0bba57898c155d
khasbin <khasbin@gmail.com>
2021-09-18 16:51:54 +0545

    Functionality for CRUD operation added

--- CRM/leads/views.py
+++ CRM/leads/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Lead, Agent, User
-from .forms import LeadForm
+from .forms import LeadForm, LeadModelForm
 
 # Create your views here.
 def lead_list(request):
@@ -19,19 +19,52 @@ def lead_detail(request, pk):
     return render(request, "leads/lead_details.html", context)
 
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == "POST":
         print("Receiving a post request")
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = Agent.objects.first()
-            Lead.objects.create(first_name = first_name, last_name= last_name,age = age, agent = agent)
-            print("The leads has been created")
+            form.save()
             return redirect('/leads')
     context = {
         "form": form
     }
     return render(request, "leads/lead_create.html", context)
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id = pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        form = LeadModelForm(request.POST, instance= lead)
+        if form.is_valid():
+            form.save()
+            return redirect('/leads')
+    context = {
+        'form':form,
+        'lead':lead
+    }
+    return render(request, "leads/lead_update.html", context)
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id = pk)
+    lead.delete()
+    return redirect('/leads')
+
+# lead = Lead.objects.get(id = pk)
+#     form = LeadForm()
+#     if request.method == 'POST':
+#         form = LeadForm(request.POST)
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             lead.first_name = first_name
+#             lead.last_name = last_name
+#             lead.age = age
+#             lead.save()
+#             return redirect('/leads')
+#     context = {
+#         "lead": lead,
+#         "form":form
+#     }
+#     return render(request, "leads/lead_update.html", context)
