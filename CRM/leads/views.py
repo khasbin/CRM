4c4d9070eed45032e0929e0dae85c6a59bba8ce5
khasbin <khasbin@gmail.com>
2021-09-22 09:27:04 +0545

    gitignore added

--- CRM/leads/views.py
+++ CRM/leads/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
-from django.views.generic.list import ListView
 from .models import Lead, Agent, User
 from .forms import LeadForm, LeadModelForm,CustomUserCreationForm
 from django.views import generic
 from django.core.mail import send_mail
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 #Create Read Update Delete + Listview
 
@@ -27,7 +26,7 @@ class LandingPageView(generic.TemplateView):
 #     return render(request, "landing.html")
 
 
-class LeadListView(generic.ListView):
+class LeadListView(LoginRequiredMixin,generic.ListView):
     template_name = "leads/lead-list.html"
     queryset = Lead.objects.all()
     context_object_name = "leads"
@@ -39,7 +38,7 @@ class LeadListView(generic.ListView):
 #     }
 #     return render(request, "leads/lead_list.html", context)
 
-class LeadDetailView(generic.DetailView):
+class LeadDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "leads/lead_details.html"
     queryset = Lead.objects.all()
     context_object_name = "lead"
@@ -53,7 +52,7 @@ class LeadDetailView(generic.DetailView):
 
 
 
-class LeadCreateView(generic.CreateView):
+class LeadCreateView(LoginRequiredMixin,generic.CreateView):
     template_name = "leads/lead_create.html"
     form_class = LeadModelForm
 
@@ -84,7 +83,7 @@ def lead_create(request):
     return render(request, "leads/lead_create.html", context)
 
 
-class LeadUpdateView(generic.UpdateView):
+class LeadUpdateView(LoginRequiredMixin,generic.UpdateView):
     template_name = "leads/lead_update.html"
     queryset = Lead.objects.all()
     form_class = LeadModelForm
@@ -109,7 +108,7 @@ def lead_update(request, pk):
     return render(request, "leads/lead_update.html", context)
 
 
-class LeadDeleteView(generic.DeleteView):
+class LeadDeleteView(LoginRequiredMixin,generic.DeleteView):
     template_name = "leads/lead_delete.html"
     queryset = Lead.objects.all()
 
