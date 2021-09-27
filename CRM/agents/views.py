47abab09c6b58749d20b3c4c21922858f167890d
khasbin <khasbin@gmail.com>
2021-09-27 21:38:33 +0545

    urls and view updated

--- CRM/agents/views.py
+++ CRM/agents/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from leads.models import Agent
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+
+class AgentListView(LoginRequiredMixin, generic.ListView):
+    template_name = "agents/agent_list.html"
+
+    def get_queryset(self):
+        return Agent.objects.all()
