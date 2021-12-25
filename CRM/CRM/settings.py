6dae89efef8a666b9b9b9e5883191ec2bc96dc36
Asbin Khatiwada <asbinkhatiwada@pop-os.localdomain>
2021-12-25 16:30:37 +0545

    environment variable set for the project

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -13,19 +13,29 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 
+import environ
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+READ_DOT_ENV_FILE = env.bool('READ_DOT_ENV_FILE', default = False)
+
+if READ_DOT_ENV_FILE:
+    environ.Env.read_env()
+
+# # Take environment variables from .env file
+# environ.Env.read_env()
+# False if not in os.environ because of casting above
+DEBUG = env('DEBUG')
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d@pi&4%prxtp(tw9f*g6h0b&kvowu)4=^*@=elr$^&sfo6txv@'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -146,6 +156,7 @@ LOGIN_REDIRECT_URL = "/leads"
 
 LOGIN_URL = "/login"
 
+LOGOUT_REDIRECT_URL = "/"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
